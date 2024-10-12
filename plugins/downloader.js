1f61a33fb00296144a21448537c3bb8659197291
D_bothack <aymericketogo@gmail.com>
2024-10-12 12:25:57 +0200

    Update downloader.js

--- plugins/downloader.js
+++ plugins/downloader.js
@@ -75,7 +75,7 @@ const {
 *Followers:* ${followers}
 *Following:* ${following}
 
-\t*KERM_MD-V2 IG STALKER*
+\t*TAKA_MD_V  IG STALKER*
 `;
 
       await m.bot.sendFromUrl(m.from, photo_profile, caption, m, {}, "image");
